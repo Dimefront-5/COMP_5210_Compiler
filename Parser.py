9f8f84fbb4ca26d00ab4fd0e16e9528020fea5b4
Tyler Ray <tylerpray32@gmail.com>
2023-09-08 13:57:03 -0500

    working with my test cases

--- Parser.py
+++ Parser.py
@@ -18,13 +18,12 @@ grammar = {
 
 
 def parser(tokens):
-    #print(tokens)
     parseTree = ASTNode("Expr")
     parseTree = parse_expr(tokens, str(0), parseTree)
     return parseTree
 
 
-def parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_addOP):
+def parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_addOP, KeyIndex):
     exprNode = ASTNode("Expr")
 
     items = list(tokens.items())
@@ -33,7 +32,7 @@ def parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_addOP):
 
     beforeAddOPTokens = dict(beforeAddOP)
     afterAddOPTokens = dict(afterAddOP)
-    #print(beforeAddOPTokens, afterAddOPTokens)
+    print(beforeAddOPTokens, afterAddOPTokens, i, index_of_addOP)
 
     exprNode = parse_expr(beforeAddOPTokens, str(i), exprNode)
 
@@ -47,7 +46,7 @@ def parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_addOP):
 
     termNode = ASTNode("Term")
     #print(afterAddOPTokens, str(index_of_addOP+1))
-    termNode = parse_term(afterAddOPTokens, str(index_of_addOP+1), termNode)
+    termNode = parse_term(afterAddOPTokens, str(int(KeyIndex)+1), termNode)
     RecentNode.add_child(termNode)
 
     return RecentNode
@@ -57,17 +56,24 @@ def parse_expr(tokens, i, RecentNode):
     i = int(i)
     index_of_addOP = 0
     index_of_last_addOP = 0
-
+    openParen = 0
+    keyIndex_For_after_token_split = str(0)
     for keyIndex in tokens:
+        print(tokens[keyIndex][1])
         if tokens[keyIndex][1] == "+" or tokens[keyIndex][1] == "-":
-            index_of_last_addOP = index_of_addOP
-            #print(index_of_last_addOP, index_of_addOP, "index of addOP")
-            addOP = tokens[keyIndex][1]
+            if openParen == 0:
+                index_of_last_addOP = index_of_addOP
+                addOP = tokens[keyIndex][1]
+                keyIndex_For_after_token_split = keyIndex
+        elif tokens[keyIndex][1] == "(":
+            openParen += 1
+        elif tokens[keyIndex][1] == ")":
+            openParen -= 1
         index_of_addOP += 1
 
     if addOP != '':
         #print("here")
-        RecentNode = parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_last_addOP)
+        RecentNode = parse_expr_addOP(tokens, i, RecentNode, addOP, index_of_last_addOP, keyIndex_For_after_token_split)
 
     else:
         termNode = ASTNode("Term")
@@ -114,11 +120,18 @@ def parse_term(tokens, i , RecentNode):
     index_of_last_mulOP = 0
 
     KeyIndex_for_after_token_split = str(0)
+
+    openParen = 0
     for keyIndex in tokens:
         if tokens[keyIndex][1] == "*" or tokens[keyIndex][1] == "/":
-            mulOP = tokens[str(keyIndex)][1]
-            index_of_last_mulOP = index_of_mulOP
-            KeyIndex_for_after_token_split = keyIndex
+            if openParen == 0:
+                mulOP = tokens[str(keyIndex)][1]
+                index_of_last_mulOP = index_of_mulOP
+                KeyIndex_for_after_token_split = keyIndex
+        elif tokens[keyIndex][1] == "(":
+            openParen += 1
+        elif tokens[keyIndex][1] == ")":
+            openParen -= 1
         index_of_mulOP += 1
         
     #print(mulOP, index_of_last_mulOP)
@@ -136,9 +149,41 @@ def parse_term(tokens, i , RecentNode):
 
     return RecentNode
 
+
+def parse_factor_paren(tokens, i, RecentNode):
+    RecentNode.add_child(ASTNode("("))
+
+    index_of_closing_paren = 0
+    isclosingparen = False
+    print(tokens)
+    for keyIndex in tokens:
+        if tokens[keyIndex][1] == ")":
+            isclosingparen = True
+            break
+        index_of_closing_paren += 1
+    print(index_of_closing_paren)
+    if not isclosingparen:
+        return False
+    
+    items = list(tokens.items())
+    between_parens = items[1:index_of_closing_paren]
+    between_parens_tokens = dict(between_parens)
+
+    print(between_parens_tokens, int(i) + 1)
+    exprNode = ASTNode("Expr")
+
+    exprNode = parse_expr(between_parens_tokens, str(int(i) + 1), exprNode)
+
+    if exprNode == False:
+        return False
+    
+    RecentNode.add_child(exprNode)
+    RecentNode.add_child(ASTNode(")"))
+    return RecentNode
+
 def parse_factor(tokens, i, RecentNode): 
     #print(i)
-    #print(tokens)
+    print(tokens)
     if tokens[i][0] == "number":
         numberNode = ASTNode(tokens[i][1])
         RecentNode.add_child(numberNode)
@@ -150,18 +195,8 @@ def parse_factor(tokens, i, RecentNode):
         return RecentNode
     
     elif tokens[i][1] == "(":
-        RecentNode.add_child(ASTNode("("))
-
-        expr = parse_expr(tokens[1:])
-        RecentNode.add_child(expr)
-
-        tokens = tokens[len(expr):]
-
-        if tokens[i][1] == ")":
-            RecentNode.add_child(ASTNode(")"))
-            return RecentNode
-        else:
-            return False
+        return parse_factor_paren(tokens, i, RecentNode)
+    
     else:
         return False
         
