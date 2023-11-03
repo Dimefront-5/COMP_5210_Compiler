3cbc2eccc6cbbf192b013ea167d259d42364c3d5
Tyler Ray <tylerpray32@gmail.com>
2023-11-03 10:11:17 -0500

    Makes basic asm for assignments

--- asmcodeshaper.py
+++ asmcodeshaper.py
@@ -165,6 +165,8 @@ def _codeShaper(codeLine):
     hasReturn = False
     if statementIndicator == 'return':
         _returnShaper(codeLine)
+    elif statementIndicator == 'assign':
+        _assignShaper(codeLine)
 
 
     if hasReturn == False:#Set Eax to 0 if there is no return statement
@@ -178,8 +180,62 @@ def _returnShaper(codeLine):
     global asmCode
 
     if re.match(cc.numbers, codeLine[0]):
-        returnCode = "mov r1, " + codeLine[0]
+        returnCode = "mov " + str(currentRegister.getRegister()) + ', ' + codeLine[0]
     else:
-        returnCode = "mov r1, [" + codeLine[0] + "]"
-        
+        returnCode = "mov " + str(currentRegister.getRegister()) + ', ' '[' + codeLine[0] + "]"
+
     asmCode.addLine(currentScope, currentBlock, returnCode)
+
+
+def _assignShaper(codeLine):
+    global currentScope
+    global currentBlock
+    global currentRegister
+    global asmCode
+
+    if re.match(cc.exprOps, codeLine[2]): #Meaning we have an expression
+        _exprShaper(codeLine)
+    else:
+        register = currentRegister.getRegister()
+        if not re.match(cc.numbers, codeLine[1]):
+            assignCode = "mov " + register + ', ' '[' + codeLine[1] + "]"
+            asmCode.addLine(currentScope, currentBlock, assignCode)
+            assignCode = "mov " + '[' + codeLine[0] + '], ' + register
+            asmCode.addLine(currentScope, currentBlock, assignCode)
+        else:
+            asmCode.addLine(currentScope, currentBlock, "mov " + '[' + codeLine[0] + '], ' + codeLine[1])
+
+
+def _exprShaper(codeLine):
+    operator = codeLine[2]
+
+    if operator == '+':
+        _operatorShaper(codeLine, 'add')
+    elif operator == '-':
+        _operatorShaper(codeLine, 'sub')
+    elif operator == '*':
+        _operatorShaper(codeLine, 'mul')
+    elif operator == '/':
+        _operatorShaper(codeLine, 'div')
+
+
+def _operatorShaper(codeLine, instruction):
+    global currentScope
+    global currentBlock
+    global currentRegister
+    global asmCode
+
+    register = currentRegister.getRegister()
+
+    if re.match(cc.numbers, codeLine[1]):
+        asmCode.addLine(currentScope, currentBlock, "mov " + register + ', ' + codeLine[1])
+    else:
+        asmCode.addLine(currentScope, currentBlock, "mov " + register + ', ' + '[' + codeLine[1] + "]")
+    
+    if re.match(cc.numbers, codeLine[3]):
+        asmCode.addLine(currentScope, currentBlock, instruction + ' ' + register + ', ' + codeLine[3])
+    else:
+        asmCode.addLine(currentScope, currentBlock, instruction + ' ' + register + ', ' + '[' + codeLine[3] + "]")
+
+    asmCode.addLine(currentScope, currentBlock, "mov " + '[' + codeLine[0] + '], ' + register)
+
